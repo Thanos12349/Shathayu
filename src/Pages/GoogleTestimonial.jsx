ac1ca9dd9c4b9cc5a9712502aff54938f5e25e3c
Thanos12349 <85013451+Thanos12349@users.noreply.github.com>
2025-11-08 19:52:23 +0530

    Update GoogleTestimonial.jsx

--- src/Pages/GoogleTestimonial.jsx
+++ src/Pages/GoogleTestimonial.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from '../Components/Header'
+import Header from '../components/Header'
 import AboutHeroSection from '../components/AboutHeroSection'
 import GoogleReviews from '../components/GoogleReviews'
-import Footer from '../Components/Footer'
+import Footer from '../components/Footer'
 
 const Testimonial = () => {
   return (
