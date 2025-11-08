5de30dccd69f7ba6031596df0a95e24f162d4cd6
Thanos12349 <85013451+Thanos12349@users.noreply.github.com>
2025-11-08 19:52:01 +0530

    Fix import paths to use lowercase 'components'

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from '../Components/Header'
+import Header from '../components/Header'
 import AboutHeroSection from '../components/AboutHeroSection'
 import ContactForm from '../components/ContactForm'
-import Footer from '../Components/Footer'
+import Footer from '../components/Footer'
 
 
 const Contact = () => {
