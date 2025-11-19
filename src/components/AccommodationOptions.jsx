1a58d226644bc8f7c520bb2bbeb46b7bb2959a8f
krishna <krishnachaudhary4405@gmail.com>
2025-11-19 17:42:34 +0530

    third

--- src/components/AccommodationOptions.jsx
+++ src/components/AccommodationOptions.jsx
@@ -6,19 +6,19 @@ const roomOptions = [
   {
     type: "Standard Room",
     description: "Our Standard Rooms provide a cozy, comfortable space designed for simplicity and relaxation. Ideal for guests seeking a peaceful rest, these rooms, which are 280 Sq.ft, feature all the essential amenities necessary for a restful stay while keeping you close to nature.",
-    imageUrl: "/images/standard-room.jpg", // Placeholder - Update path to your image
+    imageUrl: "images/offe.png", // Placeholder - Update path to your image
     altText: "Image of a Standard Room with twin beds"
   },
   {
     type: "Deluxe Room",
     description: "Step into our Deluxe Rooms for an elevated experience of comfort and style. These rooms are very spacious and offer stunning clearing views of the surrounding greenery. Deluxe rooms are 422 Sq.ft, perfect for unwinding after a day of wellness therapies.",
-    imageUrl: "/images/deluxe-room.jpg", // Placeholder - Update path to your image
+    imageUrl: "images/offe.png", // Placeholder - Update path to your image
     altText: "Image of a Deluxe Room with a king bed and lounge area"
   },
   {
     type: "Luxury Room",
     description: "For those seeking the finest experience, our Luxury Rooms offer unmatched elegance and tranquility. These rooms, which are 543 Sq.ft, feature luxurious interiors, modern amenities, and private balcony facing lake and hills for relaxation and rejuvenation.",
-    imageUrl: "/images/luxury-room.jpg", // Placeholder - Update path to your image
+    imageUrl: "images/offe.png", // Placeholder - Update path to your image
     altText: "Image of a Luxury Room with a large balcony and view"
   },
 ];
